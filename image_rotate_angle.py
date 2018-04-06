9b1286cadafa4d7a187cb6218c421674624e1eb7
mihir <phatak.mihir@gmail.com>
2018-04-06 02:34:18 -0400

    Code complete with Optimization Removed Unused Variables

--- image_rotate_angle.py
+++ image_rotate_angle.py
@@ -10,17 +10,15 @@ import sys                                        #Used for accepting commadnlin
 
 #Function That Returns  Correction Angle
 def find_correction_angle(input_csv_file):
- row_counter = 0
- column_counter = 0
  list_of_grid_elements = []
  main_list = []
  updated_csv_file = input_csv_file + '_updated.csv'
  
- with open(input_csv_file,'r') as f:               #Nested Context Manager for reading original CSV File
-  with open(updated_csv_file,'w') as f1:
+ with open(input_csv_file, 'r') as f:               #Nested Context Manager for reading original CSV File
+  with open(updated_csv_file, 'w') as f_new:
        f.readline() 
        for line in f:
-           f1.write(line) 
+           f_new.write(line) 
 
  with open(updated_csv_file, 'r') as f:            #Nested Context Manager for reading updated CSV file with first row labels removed'''
   reader = csv.reader(f)
@@ -35,15 +33,11 @@ def find_correction_angle(input_csv_file):
 
  #Return LeftMost Edge Co-ordinates of the Rectangle 
  co_ordinates_of_shape = []
- four_corners_of_rectangle = []
- single_point_coordinates = []
  for single_element in main_list:                   #Seperating Element Point(pixels) of White Shape
   if single_element[2] == 1:
    co_ordinates_of_shape.append(single_element)
 
  temp_highest_row = 0
- temp_highest_col = 0
- temp_lowest_row = 10000000
  temp_lowest_col = 10000000
  for black_pixel in co_ordinates_of_shape:          #Finding End-Points of Leftmost Edge Of Rectangle
 
@@ -52,14 +46,14 @@ def find_correction_angle(input_csv_file):
 
   if(black_pixel[0] == temp_highest_row):
    temp_highest_row_cc_col = black_pixel[1]
-   Y1,X1 = temp_highest_row, temp_highest_row_cc_col #Point with highest row co-ordinate (y co-ordinate)
+   Y1, X1 = temp_highest_row, temp_highest_row_cc_col #Point with highest row co-ordinate (y co-ordinate)
 
   if(black_pixel[1] < temp_lowest_col):
    temp_lowest_col = black_pixel[1]
 
   if(black_pixel[1] == temp_lowest_col):
    temp_lowest_col_cc_row = black_pixel[0]
-   Y2,X2 = temp_lowest_col_cc_row,temp_lowest_col    #Point with lowest column co-ordinate (x co-ordinate)
+   Y2, X2 = temp_lowest_col_cc_row, temp_lowest_col    #Point with lowest column co-ordinate (x co-ordinate)
 
  slope = (Y2-Y1)/(X2-X1)                             #Calculating Slope from two leftmost points
  '''
@@ -73,5 +67,5 @@ def find_correction_angle(input_csv_file):
 if __name__ == "__main__": 
  input_csv_file = sys.argv[1]
  correction_angle = find_correction_angle(input_csv_file) 
- print("\nCorrection Angle is : ",correction_angle)
+ print("\n Correction Angle is : ", correction_angle)
  print("\n")
