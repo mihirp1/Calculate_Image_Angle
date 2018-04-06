6548a8cfb1ad42fc4117c0c341078cb526080c60
mihir <phatak.mihir@gmail.com>
2018-04-06 02:52:46 -0400

    Cosmetic Changes

--- image_rotate_angle.py
+++ image_rotate_angle.py
@@ -14,15 +14,16 @@ def find_correction_angle(input_csv_file):
  main_list = []
  updated_csv_file = input_csv_file.split('.csv')[0] + '_updated.csv'
  
- with open(input_csv_file, 'r') as f:               #Nested Context Manager for reading original CSV File
+ with open(input_csv_file, 'r') as f_old:         #Nested Context Manager for reading original CSV File
   with open(updated_csv_file, 'w') as f_new:
-       f.readline() 
-       for line in f:
+       f_old.readline() 
+       for line in f_old:
            f_new.write(line) 
-
- with open(updated_csv_file, 'r') as f:            #Nested Context Manager for reading updated CSV file with first row labels removed'''
-  reader = csv.reader(f)
-  for row_number,column in enumerate(reader):      #Creation Of Three Element List for co-ordinates and values : (X,Y,Pixel-Value)'''
+  f_new.close()
+ f_old.close()
+ with open(updated_csv_file, 'r') as f_newest:    #Nested Context Manager for reading updated CSV file with first row labels removed'''
+  reader = csv.reader(f_newest)
+  for row_number,column in enumerate(reader):     #Creation Of Three Element List for co-ordinates and values : (X,Y,Pixel-Value)'''
     for column_number,element in enumerate(column):
      if(not element.startswith('V')):
        list_of_grid_elements.append(int(row_number)+1)
@@ -30,16 +31,16 @@ def find_correction_angle(input_csv_file):
        list_of_grid_elements.append(int(column[column_number]))
        main_list.append(list_of_grid_elements)
        list_of_grid_elements  = []
-
+ f_newest.close()
  #Return LeftMost Edge Co-ordinates of the Rectangle 
  co_ordinates_of_shape = []
- for single_element in main_list:                   #Seperating Element Point(pixels) of White Shape
+ for single_element in main_list:                 #Seperating Element Point(pixels) of White Shape
   if single_element[2] == 1:
    co_ordinates_of_shape.append(single_element)
 
  temp_highest_row = 0
  temp_lowest_col = 10000000
- for black_pixel in co_ordinates_of_shape:          #Finding End-Points of Leftmost Edge Of Rectangle
+ for black_pixel in co_ordinates_of_shape:        #Finding End-Points of Leftmost Edge Of Rectangle
 
   if(black_pixel[0] > temp_highest_row):
    temp_highest_row = black_pixel[0]
